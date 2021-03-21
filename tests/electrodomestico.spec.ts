dd76ffaa09ed67a6622b1cf63136626afd6d1339
Adrian Hernandez Suarez <alu0101235516@ull.edu.es>
2021-03-21 16:07:46 +0000

    Creacion de los ficheros del Ejercicio 1- Pokedex

--- tests/electrodomestico.spec.ts
+++ tests/electrodomestico.spec.ts
@@ -1,3 +1,4 @@
+/*
 import 'mocha';
 import {expect} from 'chai';
 import {Electrodomestico} from '../src/electrodomestico';
@@ -20,3 +21,4 @@ describe('TEST LAVADORA', () => {
     return expect(lavadora.precioLavadora()).to.be.equal(230);
   });
 });
+*/
