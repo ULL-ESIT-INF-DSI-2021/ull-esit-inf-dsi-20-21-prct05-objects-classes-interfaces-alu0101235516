08b66dadd87fb0e9328b6369aa1936c223872fed
Adrian Hernandez Suarez <alu0101235516@ull.edu.es>
2021-03-21 18:43:21 +0000

    Creacion de la clase Bicicleta

--- src/ejercicio-3/bicicleta.ts
+++ src/ejercicio-3/bicicleta.ts
@@ -1,1 +1,17 @@
+import {Vehiculo} from "./vehiculo";
 
+/**
+ * Clase Bicicleta
+ * Hereda de la clase Vehiculo
+ */
+
+export class Bicicleta extends Vehiculo {
+  /**
+   * Constructor de la clase
+   * @param velocity Velocidad a la que se mueve el vehiculo
+   * @param dimensions Dimensiones del vehiculo
+   */
+  constructor(velocity: number, dimensions: string) {
+    super(velocity, dimensions);
+  }
+}
