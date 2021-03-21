4a0b53df5f0f5a08cbfc43b4f38e77b6f3301314
Adrian Hernandez Suarez <alu0101235516@ull.edu.es>
2021-03-21 18:42:55 +0000

    Creacion de la clase vehiculo

--- src/ejercicio-3/vehiculo.ts
+++ src/ejercicio-3/vehiculo.ts
@@ -1,1 +1,15 @@
+import {Movable} from "./movable";
 
+/**
+ * Clase Vehiculo (Padre)
+ * Implemente la interfaz Movable
+ */
+
+export class Vehiculo implements Movable {
+  /**
+   * Constructor de la clase
+   * @param velocity Velocidad a la que se mueve el vehiculo
+   * @param dimensions Dimensiones del vehiculo
+   */
+  constructor(public velocity: number, public dimensions: string) {}
+}
